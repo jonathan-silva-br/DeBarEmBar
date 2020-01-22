b5e8d8c6b2af9c5332120dc27d0a08c9e0a1fcf6
Jonathan Silva <silva_jonathan@outlook.com.br>
2020-01-22 09:54:33 -0300

    Layout fragment_cadastro.xml

--- app/src/main/java/com/example/debarembar/view/ui/cadastro/CadastroFragment.java
+++ app/src/main/java/com/example/debarembar/view/ui/cadastro/CadastroFragment.java
@@ -23,7 +23,7 @@ public class CadastroFragment extends Fragment {
         cadastroViewModel =
                 ViewModelProviders.of(this).get(CadastroViewModel.class);
         View root = inflater.inflate(R.layout.fragment_cadastro, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
+        final TextView textView = root.findViewById(R.id.textTituloView);
         cadastroViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
