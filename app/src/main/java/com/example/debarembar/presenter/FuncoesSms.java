a69d4125cef595dc7259414cd421b85cab5e8a02
Matheus Geiser <matheusgeiser@gmail.com>
2020-01-21 21:51:42 -0300

    WIP
    Implementada a classe e algumas funções para realizar o envio do sms com as informações dos bares.

---
+++ app/src/main/java/com/example/debarembar/presenter/FuncoesSms.java
@@ -0,0 +1,66 @@
+package com.example.debarembar.presenter;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+/**
+ * Classe responsável pelas funções de SMS
+ *
+ * @author Matheus Geiser <matheusgeiser@gmail.com>
+ * @since 1.0.0
+ *
+ */
+public class FuncoesSms {
+
+    /**
+     * TODO >>> https://www.youtube.com/watch?v=Z28s39brZJM <<<
+     */
+    final int SEND_SMS_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Método responsável por enviar o SMS
+     *
+     * Através do método checkpermission, se tiver a permissão necessária
+     * ele irá utilizar o smsManager para enviar uma mensagem para o número
+     * informado no parâmetro com o conteúdo do {@param mensagem}.
+     *
+     * @author Matheus Geiser <matheusgeiser@gmail.com>
+     * @since 1.0.0
+     *
+     * @param c - contexto do aplicativo
+     * @param numero - numero do telefone que estou enviando
+     * @param mensagem - conteúdo do SMS
+     */
+    public void onSend(Context c, String numero, String mensagem){
+
+        if(checkPermission(Manifest.permission.SEND_SMS, c)){
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(numero,null,mensagem,null,null);
+            Toast.makeText(c, "Compartilhado!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /**
+     *
+     * Método que ve se tem a permissão necessária
+     *
+     * @author Matheus Geiser <matheusgeiser@gmail.com>
+     * @since 1.0.0
+     *
+     * @param permission
+     * @param context
+     * @return true se houver permissão, se não ele retorna false
+     */
+    public boolean checkPermission(String permission, Context context){
+        int check = ContextCompat.checkSelfPermission(context,permission);
+        return (check == PackageManager.PERMISSION_GRANTED);
+    }
+
+
+}
